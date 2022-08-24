571ed53d46450d2935c47f90a71f2a6e376ae0b8
xbbyhiras <xavier.brinon@byhiras.com>
2022-08-24 08:48:34 +0000

    build: :sparkles: generate nx project with ci build

---
+++ nx-odyssey/apps/cats-in-space/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="cats-in-space" />
+      <div />
+    </>
+  );
+}
+
+export default App;
